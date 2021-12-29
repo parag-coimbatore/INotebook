1e9ab8d763db0c10d2f6c3469444d6b15b37821d
Parag Coimbatore <paragcoimbatore745@gmail.com>
2021-12-29 22:35:13 +0530

    Navbar of iNotebook created

--- src/App.js
+++ src/App.js
@@ -1,10 +1,39 @@
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes
+} from "react-router-dom";
+import Navbar from './components/Navbar';
+import Home from './components/Home';
+import About from './components/About';
 
 function App() {
   return (
-    <div className="App">
-      <h1>This is iNotebook</h1>
-    </div>
+    <>
+      <Navbar/>
+      <Routes>
+        <Route exact path='/' element={<Home />}></Route>
+
+        <Route exact path='/about' element={<About />}></Route>
+
+        
+      </Routes>
+      {/* <Router>
+        <Navbar />
+        <Home />
+
+        <Routes>
+          <Route path="/">
+            <Home />
+          </Route>
+          <Route exact path="/about">
+            <About/>
+          </Route>
+
+        </Routes>
+      </Router> */}
+    </>
   );
 }
 
